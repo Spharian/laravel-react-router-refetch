40c00ccfd705d8c08b9288d755ede2066cf21b11
Spharian <s.fertons@gmail.com>
2016-02-23 18:16:14 +0100

    Remove useless defaults

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,15 +11,6 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
-});
-
 $factory->define(App\Post::class, function (Faker\Generator $faker) {
     return [
         'title' => $faker->realText($faker->numberBetween(10,20)),
